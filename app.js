d22b81949138ca7613234c2eba9e1a5400d7f926
Yurika Namba <yurikanamba@gmail.com>
2020-04-27 08:06:13 +0900

    added some styling

--- app.js
+++ app.js
@@ -33,27 +33,27 @@ function loadData() {
           body.innerHTML = postdata.body;
           post.appendChild(body);
 
-          const status = document.createElement("p");
-          status.classList.add("status");
-          status.innerHTML = `status: ${postdata.status}`;
-          post.appendChild(status);
+          // const status = document.createElement("p");
+          // status.classList.add("status");
+          // status.innerHTML = `status: ${postdata.status}`;
+          // post.appendChild(status);
 
           const timestamp = document.createElement("p");
           timestamp.classList.add("timestamp");
           timestamp.innerHTML = postdata.time;
           post.appendChild(timestamp);
 
-          const deleteButton = document.createElement("button");
-          deleteButton.classList.add("delete-btn");
-          deleteButton.id = postdata.id;
-          deleteButton.innerHTML = "🗑️";
-          post.appendChild(deleteButton);
-
           const editButton = document.createElement("button");
           editButton.classList.add("edit-btn");
           editButton.id = postdata.id;
           editButton.innerHTML = "✏️";
           post.appendChild(editButton);
+
+          const deleteButton = document.createElement("button");
+          deleteButton.classList.add("delete-btn");
+          deleteButton.id = postdata.id;
+          deleteButton.innerHTML = "🗑️";
+          post.appendChild(deleteButton);
         }
       });
     })
@@ -111,27 +111,27 @@ function postsByStatus(e) {
         body.innerHTML = postdata.body;
         post.appendChild(body);
 
-        const status = document.createElement("p");
-        status.classList.add("status");
-        status.innerHTML = `status: ${postdata.status}`;
-        post.appendChild(status);
+        // const status = document.createElement("p");
+        // status.classList.add("status");
+        // status.innerHTML = `status: ${postdata.status}`;
+        // post.appendChild(status);
 
         const timestamp = document.createElement("p");
         timestamp.classList.add("timestamp");
         timestamp.innerHTML = postdata.time;
         post.appendChild(timestamp);
 
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete-btn");
-        deleteButton.id = postdata.id;
-        deleteButton.innerHTML = "🗑️";
-        post.appendChild(deleteButton);
-
         const editButton = document.createElement("button");
         editButton.classList.add("edit-btn");
         editButton.id = postdata.id;
         editButton.innerHTML = "✏️";
         post.appendChild(editButton);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-btn");
+        deleteButton.id = postdata.id;
+        deleteButton.innerHTML = "🗑️";
+        post.appendChild(deleteButton);
       });
     })
     .then(() => {
@@ -174,17 +174,21 @@ function deletePost(e) {
 //EDIT POST
 function editPost(e) {
   const id = e.target.id;
+  const titleAndBody = document.querySelector(`.post-${id}`).childNodes;
+  const titlePlaceHolder = titleAndBody[0].innerText;
+  const bodyPlaceHolder = titleAndBody[1].innerText;
+  console.log(titlePlaceHolder, bodyPlaceHolder);
+
+  document.getElementById("edit-post-title").placeHolder = titlePlaceHolder;
+  document.getElementById("edit-post-body").placeHolder = bodyPlaceHolder;
 
   //display form
   const editPostForm = document.getElementById("popup");
-  editPostForm.style.display = "block";
+  editPostForm.style.display = "flex";
 
   //add save button
   const post = document.querySelector(`.post-${id}`);
-  const saveBtn = document.createElement("button");
-  saveBtn.innerHTML = "💾";
-  saveBtn.classList.add("save-edit");
-  post.appendChild(saveBtn);
+  const saveBtn = document.getElementById("edit-post-btn");
 
   saveBtn.addEventListener("click", () => {
     //get input values
@@ -193,6 +197,14 @@ function editPost(e) {
     const statusDropdown = document.getElementById("edit-post-status");
     const statusVal = statusDropdown.options[statusDropdown.selectedIndex].text;
     editPostForm.style.display = "none";
+
+    // if (!titleVal && titlePlaceHolder) {
+    //   titleVal = titlePlaceHolder;
+    // }
+
+    // if (!bodyVal && bodyPlaceHolder) {
+    //   bodyVal = bodyPlaceHolder;
+    // }
 
     const mutation = `
   mutation{
@@ -249,6 +261,7 @@ function addPost() {
   })
     .then((res) => res.json())
     .then((res) => console.log(res))
+    .then(() => loadData())
     .catch((err) => console.log(err));
 }
 
